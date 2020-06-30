b496cf407b35d9e7f85175bb57362ab5115cf45f
Pyrax <piranhadev@gmail.com>
2020-06-30 19:16:54 +0200

    fix: note about empty details with reference to docs

--- gatsby-theme-contact/src/components/details.js
+++ gatsby-theme-contact/src/components/details.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Details = () => (
   <>
-    <p>No details specified.</p>
+    <h3>No details specified</h3>
+    <p>
+      If you are the owner of this site, you can specify details by overriding
+      this file. Have a look at the README of gatsby-theme-contact for how this
+      is done.
+    </p>
   </>
 )
 
